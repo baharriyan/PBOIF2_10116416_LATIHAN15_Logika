36c2721bdbe2744c37333f62b3610d00d3a14c2f
baharriyan <indonesiariyan@gmail.com>
2020-10-20 19:07:06 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN15_Logika/src/pboif2_10116416_latihan15_logika/PBOIF2_10116416_LATIHAN15_Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan15_logika;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_LATIHAN15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
